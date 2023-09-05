28523b8993f94818f5f5f1d9631307c9e88c5e40
ramiz-bozai-dbt <ramiz.bozai@dbtlabs.com>
2023-09-05 10:13:56 -0400

    add conference mapping model and metrics

---
+++ models/staging/nfl/stg_conference_mapping.sql
@@ -0,0 +1,17 @@
+with source as (
+
+    select * from {{ source('nfl', 'conference_mapping') }}
+
+),
+
+renamed as (
+
+    select
+        tm as team,
+        conf as conference
+
+    from source
+
+)
+
+select * from renamed
